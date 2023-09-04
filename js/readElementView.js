866104b1265aa6832974d9bd945d1b53f0aa094c
FarisBektasevic <bektasevicfaris@gmail.com>
2023-09-04 06:55:00 +0200

    bcg final

--- js/readElementView.js
+++ js/readElementView.js
@@ -9,9 +9,9 @@ const readElementMarkup = (element, prev, next) => {
   };
 
   return `
-    <div class="sidebar__header background-${
-      element.name
-    }" style="border-color:${groupBlockColors[element.groupBlock]}">
+    <div class="sidebar__header background-${element.groupBlock
+      .split(' ')
+      .join('-')}" style="border-color:${groupBlockColors[element.groupBlock]}">
         <div class="header__buttons">
             <i class="fa-solid fa-chevron-left close-sidebar"></i>
             <div class="wiki-link">
